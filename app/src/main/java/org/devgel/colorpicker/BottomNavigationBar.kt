e9e4d6b0a460b78855a8c0e54d1d5c1cfd920d29
“Aaron <amangel@gmail.com>
2025-07-02 12:37:01 -0600

    Initial content

---
+++ app/src/main/java/org/devgel/colorpicker/BottomNavigationBar.kt
@@ -0,0 +1,54 @@
+package org.devgel.colorpicker
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        NavigationItem.Colors,
+        NavigationItem.Camera,
+        NavigationItem.Placeholder
+    )
+    NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
+                label = { Text(text = item.title) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        navController.graph.startDestinationRoute?.let {
+                            popUpTo(it) {
+                                saveState = true
+                            }
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+sealed class NavigationItem(var route: String, var icon: Int, var title: String) {
+    object Colors : NavigationItem("colors", R.drawable.ic_colors, "Colors")
+    object Camera : NavigationItem("camera", R.drawable.ic_camera, "Camera")
+    object Placeholder : NavigationItem("placeholder", R.drawable.ic_placeholder, "Placeholder")
+}
